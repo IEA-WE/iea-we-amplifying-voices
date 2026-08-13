1549f0545575646cdc6e05fbdb7076c478baddde
IEA-WE <internationaliea@iea-world.org>
2026-08-13 13:09:26 +0200

    Add new articles for 2024 to articles-data.js

--- articles-data.js
+++ articles-data.js
@@ -157,6 +157,395 @@ window.articles = [
     topic: "Development",
     url: "https://www.project-syndicate.org/commentary/colombia-drug-policy-reform-by-maria-alejandra-velez-2023-12?barrier=accesspaylog"
 
-  }
+  },
+
+  // =========================
+  // 2024
+  // =========================
+
+  {
+  title: "Sri Lanka’s Debt Restructuring Is Hurting Older Women",
+  author: "Nisha Arunatilake",
+  publication: "PS",
+  date: "10-Jan",
+  year: 2024,
+  country: "Sri Lanka",
+  topic: "",
+  url: "https://www.project-syndicate.org/commentary/sri-lanka-domestic-debt-restructuring-reduces-women-pension-by-nisha-arunatilake-2024-01"
+},
+
+{
+  title: "Are Social Norms Really the Main Cause of Low Female Employment?",
+  author: "Ashwini Deshpande",
+  publication: "PS",
+  date: "16-Jan",
+  year: 2024,
+  country: "India",
+  topic: "",
+  url: "https://www.project-syndicate.org/commentary/social-norms-not-only-cause-india-low-women-employment-rate-by-ashwini-deshpande-2024-01"
+},
+
+{
+  title: "Will Turkey Remain Committed to Economic Reform?",
+  author: "Selva Demiralp",
+  publication: "PS",
+  date: "05-Feb",
+  year: 2024,
+  country: "Turkey",
+  topic: "",
+  url: "https://www.project-syndicate.org/commentary/will-turkey-erdogan-continue-to-pursue-orthodox-economic-policies-by-selva-demiralp-2024-02"
+},
+
+{
+  title: "How did child health respond to trade shocks in Brazil?",
+  author: "Danyelle Branco",
+  publication: "VoxDev",
+  date: "08-Feb",
+  year: 2024,
+  country: "Brazil",
+  topic: "Health",
+  url: "https://voxdev.org/topic/health/how-did-child-health-respond-trade-shocks-brazil"
+},
+
+{
+  title: "Rethinking Inequality in Latin America",
+  author: "Ana María Ibáñez",
+  publication: "PS",
+  date: "20-Feb",
+  year: 2024,
+  country: "Mexico",
+  topic: "",
+  url: "https://www.project-syndicate.org/commentary/latin-america-inequality-needs-radical-rethink-by-ana-maria-ibanez-2024-02"
+},
+
+{
+  title: "Victim-blaming norms and violence against women: Moral considerations can induce policy and behaviour change",
+  author: "Sevinç Bermek and Aslı Unan",
+  publication: "VoxEU",
+  date: "08-Mar",
+  year: 2024,
+  country: "Turkey and UK",
+  topic: "Gender",
+  url: "https://cepr.org/voxeu/columns/victim-blaming-norms-and-violence-against-women-moral-considerations-can-induce"
+},
+
+{
+  title: "Beyond rejection: Exploring gender differences in academic resilience",
+  author: "Renata Narita, Paula Pereda, Fabiana Rocha, Maria Dolores Montoya Diaz",
+  publication: "VoxEU",
+  date: "08-Mar",
+  year: 2024,
+  country: "Brazil",
+  topic: "Gender",
+  url: "https://cepr.org/voxeu/columns/beyond-rejection-exploring-gender-differences-academic-resilience"
+},
+
+{
+  title: "How Wide Are Latin America’s Health Inequalities?",
+  author: "Dolores de la Mata",
+  publication: "PS",
+  date: "14-Mar",
+  year: 2024,
+  country: "Argentina",
+  topic: "Health",
+  url: "https://www.project-syndicate.org/commentary/latin-america-large-health-inequalities-by-dolores-de-la-mata-2024-03"
+},
+
+{
+  title: "Give Women with Disabilities a Chance",
+  author: "Nkechi Owoo",
+  publication: "PS",
+  date: "15-Mar",
+  year: 2024,
+  country: "Ghana",
+  topic: "Gender",
+  url: "https://www.project-syndicate.org/commentary/helping-women-with-disabilities-escape-poverty-and-achieve-social-inclusion-by-nkechi-s-owoo-2024-03"
+},
+
+{
+  title: "The world’s largest humanitarian cash transfer programme reduces child labour and increases schooling among refugee children",
+  author: "Aysun Hızıroğlu Aygün",
+  publication: "VoxEU",
+  date: "25-Mar",
+  year: 2024,
+  country: "Turkey",
+  topic: "Education",
+  url: "https://cepr.org/voxeu/columns/worlds-largest-humanitarian-cash-transfer-programme-reduces-child-labour-and"
+},
+
+{
+  title: "Africa’s Prosperity Depends on Achieving Gender Equality",
+  author: "Jane Kabubo-Mariara",
+  publication: "PS",
+  date: "27-Mar",
+  year: 2024,
+  country: "Kenya",
+  topic: "Gender",
+  url: "https://www.project-syndicate.org/commentary/empowering-african-women-in-the-labor-market-by-jane-kabubo-mariara-2024-03"
+},
+
+{
+  title: "Silence is golden: How public criticism of central banks can backfire for leaders",
+  author: "Selva Demiralp",
+  publication: "VoxEU",
+  date: "18-Apr",
+  year: 2024,
+  country: "Turkey",
+  topic: "Macroeconomics",
+  url: "https://cepr.org/voxeu/columns/silence-golden-how-public-criticism-central-banks-can-backfire-leaders"
+},
+
+{
+  title: "Exposure to soap operas featuring LGBTQ+ characters has a negative short-term impact on viewers’ attitudes towards homosexuality",
+  author: "Alejandra Ramos and María Lombardi",
+  publication: "VoxEU",
+  date: "20-Apr",
+  year: 2024,
+  country: "Argentina and Colombia",
+  topic: "Gender",
+  url: "https://cepr.org/voxeu/columns/exposure-soap-operas-featuring-lgbtq-characters-has-negative-short-term-impact"
+},
+
+{
+  title: "As families change, so must the safety net",
+  author: "Neha Bansal",
+  publication: "PS",
+  date: "22-Apr",
+  year: 2024,
+  country: "India",
+  topic: "Social Policy",
+  url: "https://www.project-syndicate.org/commentary/how-to-strengthen-family-intergenerational-solidarity-by-neha-bansal-2024-04"
+},
+
+{
+  title: "Entering during an economic crisis: Labour market trajectories of young Indian workers",
+  author: "Mrinalini Jha and Rosa Abraham",
+  publication: "VoxEU",
+  date: "29-May",
+  year: 2024,
+  country: "India",
+  topic: "Labour Markets",
+  url: "https://cepr.org/voxeu/columns/entering-during-economic-crisis-labour-market-trajectories-young-indian-workers"
+},
+
+{
+  title: "Capital controls and firm performance: The Chilean ‘encaje’",
+  author: "Eugenia Andreasen, Sofía Bauducco, Evangelina Dardati",
+  publication: "VoxEU",
+  date: "07-Jun",
+  year: 2024,
+  country: "Chile",
+  topic: "Macroeconomics",
+  url: "https://cepr.org/voxeu/columns/capital-controls-and-firm-performance-chilean-encaje"
+},
+
+{
+  title: "Heatwaves ripple into the financial system",
+  author: "Sandra Aguilar-Gomez",
+  publication: "VoxEU",
+  date: "12-Jun",
+  year: 2024,
+  country: "Colombia",
+  topic: "Climate",
+  url: "https://cepr.org/voxeu/columns/heatwaves-ripple-financial-system"
+},
+
+{
+  title: "Can Private Hospitals Increase Access to Health Care in Developing Countries?",
+  author: "Tanika Chakraborty and Titir Bhattacharya",
+  publication: "PS",
+  date: "17-Jun",
+  year: 2024,
+  country: "India",
+  topic: "Health",
+  url: "https://www.project-syndicate.org/commentary/unlocking-benefits-of-social-health-insurance-in-low-and-middle-income-countries-by-titir-bhattacharya-and-tanika-chakraborty-2024-06"
+},
+
+{
+  title: "Affordable home ownership can lead to stronger relationships",
+  author: "Jeanne Lafortune and Corinne Low",
+  publication: "VoxEU",
+  date: "20-Jun",
+  year: 2024,
+  country: "Chile and US",
+  topic: "Gender",
+  url: "https://cepr.org/voxeu/columns/affordable-home-ownership-can-lead-stronger-relationships"
+},
+
+{
+  title: "Using nudges to improve food choices",
+  author: "Ana Balsa, Cecilia Noboa, Patricia Triunfo",
+  publication: "VoxEU",
+  date: "27-Jun",
+  year: 2024,
+  country: "Uruguay",
+  topic: "Health",
+  url: "https://cepr.org/voxeu/columns/using-nudges-improve-food-choices"
+},
+
+{
+  title: "Rethinking How to Address Irregular Migration from Africa",
+  author: "Linguère Mously Mbaye",
+  publication: "PS",
+  date: "28-Jun",
+  year: 2024,
+  country: "Ivory Coast",
+  topic: "Migration",
+  url: "https://www.project-syndicate.org/commentary/africa-needs-a-new-migration-narrative-by-linguere-mously-mbaye-2024-06"
+},
+
+{
+  title: "High Labor Turnover Fuels Inequality in Latin America",
+  author: "Renata Narita",
+  publication: "PS",
+  date: "01-Aug",
+  year: 2024,
+  country: "Brazil",
+  topic: "Labour Markets",
+  url: "https://www.project-syndicate.org/commentary/frequent-job-transitions-exacerbate-latin-america-wage-inequality-by-renata-narita-2024-08"
+},
+
+{
+  title: "The impact of road connectivity on fertility and child health in rural India",
+  author: "Aparajita Dasgupta",
+  publication: "VoxEU",
+  date: "07-Aug",
+  year: 2024,
+  country: "India",
+  topic: "Health",
+  url: "https://cepr.org/voxeu/columns/impact-road-connectivity-fertility-and-child-health-rural-india"
+},
+
+{
+  title: "Uruguay’s Lessons for Building a Care Economy",
+  author: "Carmen Estrades and Florencia Amabile",
+  publication: "PS",
+  date: "13-Aug",
+  year: 2024,
+  country: "Uruguay",
+  topic: "Care Economy",
+  url: "https://www.project-syndicate.org/commentary/uruguay-aging-society-low-fertility-rate-calls-for-robust-care-provision-by-carmen-estrades-1-and-florencia-amabile-2024-08"
+},
+
+{
+  title: "Rethinking Emissions Targets in Developing Countries",
+  author: "Elisa Belfiori and Yanel Llohis",
+  publication: "PS",
+  date: "27-Aug",
+  year: 2024,
+  country: "Argentina",
+  topic: "Climate",
+  url: "https://www.project-syndicate.org/commentary/developing-countries-need-different-net-emissions-targets-climate-policy-by-elisa-belfiori-and-yanel-llohis-2024-08"
+},
+
+{
+  title: "The urban wage premium for women: Evidence across the wage distribution",
+  author: "Solange Ledi Gonçalves and Eloiza Almeida",
+  publication: "VoxEU",
+  date: "05-Sep",
+  year: 2024,
+  country: "Brazil",
+  topic: "Labour Markets",
+  url: "https://cepr.org/voxeu/columns/urban-wage-premium-women-evidence-across-wage-distribution"
+},
+
+{
+  title: "Egypt’s Skyrocketing C-Sections",
+  author: "Rana Hendy and Lobna Shaheen",
+  publication: "PS",
+  date: "11-Sep",
+  year: 2024,
+  country: "Egypt",
+  topic: "Health",
+  url: "https://www.project-syndicate.org/commentary/egypt-rise-cesarean-births-driven-by-economic-turmoil-private-health-by-rana-hendy-1-and-lobna-shaheen-2024-09"
+},
+
+{
+  title: "School feeding programmes improved attendance in northern Burkina Faso",
+  author: "Pouirkèta Rita Nikiema",
+  publication: "VoxDev",
+  date: "17-Sep",
+  year: 2024,
+  country: "Burkina Faso",
+  topic: "Education",
+  url: "https://voxdev.org/topic/education/school-feeding-programmes-improved-attendance-northern-burkina-faso"
+},
+
+{
+  title: "Unfair rentals: Discrimination against transgender couples in Latin America",
+  author: "María Florencia Pinto, Mariana Marchionni and Inés Berniell",
+  publication: "VoxDev",
+  date: "26-Sep",
+  year: 2024,
+  country: "Argentina",
+  topic: "Gender",
+  url: "https://voxdev.org/topic/institutions-political-economy/unfair-rentals-discrimination-against-transgender-couples"
+},
+
+{
+  title: "Addressing the lingering effects of colonial influence on educational institutions",
+  author: "Maleke Fourati",
+  publication: "VoxEU",
+  date: "30-Sep",
+  year: 2024,
+  country: "Tunisia and Tahiti",
+  topic: "Education",
+  url: "https://cepr.org/voxeu/columns/addressing-lingering-effects-colonial-influence-educational-institutions"
+},
+
+{
+  title: "Bridging global divides: How Southern researchers can lead sustainable development",
+  author: "Verónica Amarante, Jane Mariara, Ana Maria Kassouf",
+  publication: "VoxDev",
+  date: "25-Oct",
+  year: 2024,
+  country: "Kenya, Uruguay, Brazil",
+  topic: "Development",
+  url: "https://voxdev.org/topic/bridging-global-divides-how-southern-researchers-can-lead-sustainable-development"
+},
+
+{
+  title: "To Prosper, India Must Close Its Gender Employment Gap",
+  author: "Kanika Mahajan",
+  publication: "PS",
+  date: "05-Nov",
+  year: 2024,
+  country: "India",
+  topic: "Labour Markets",
+  url: "https://www.project-syndicate.org/commentary/creating-conditions-for-female-employment-in-india-by-kanika-mahajan-2024-11"
+},
+
+{
+  title: "The Second Glass Ceiling for Women",
+  author: "Xianghong Wang",
+  publication: "PS",
+  date: "05-Dec",
+  year: 2024,
+  country: "China",
+  topic: "Gender",
+  url: "https://www.project-syndicate.org/commentary/china-must-end-gender-specific-mandatory-retirement-age-by-xianghong-wang-2024-12"
+},
+
+{
+  title: "The Bank of Korea Moves Beyond Monetary Policy",
+  author: "Soohyung Lee",
+  publication: "PS",
+  date: "12-Dec",
+  year: 2024,
+  country: "South Korea",
+  topic: "Macroeconomics",
+  url: "https://www.project-syndicate.org/commentary/bank-of-korea-addresses-structural-challenges-rising-housing-costs-by-soohyung-lee-2024-12"
+},
+
+{
+  title: "Crime Is Punishing Latin America’s Economies",
+  author: "Ana María Ibáñez and Nathalie Alvarado",
+  publication: "PS",
+  date: "17-Dec",
+  year: 2024,
+  country: "Mexico",
+  topic: "Development",
+  url: "https://www.project-syndicate.org/commentary/latin-america-crime-crisis-economic-consequences-by-nathalie-alvarado-and-ana-maria-ibanez-2024-12"
+}
 
 ];
